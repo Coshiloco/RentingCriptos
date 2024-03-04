92bce1d9e67e6ad78a65e59ef0103d3422290fb5
Coshiloco <pablohurtadohg@gmail.com>
2024-03-04 01:45:57 +0100

    Commit to Auth

--- server/src/main/kotlin/com/rentingcriptos/kmp/Application.kt
+++ server/src/main/kotlin/com/rentingcriptos/kmp/Application.kt
@@ -1,14 +1,13 @@
 package com.rentingcriptos.kmp
 
-import Greeting
 import SERVER_PORT
+import com.rentingcriptos.kmp.plugins.configureRouting
+import com.rentingcriptos.kmp.plugins.configureSecurity
+import com.rentingcriptos.kmp.plugins.configureSerialization
+import com.rentingcriptos.kmp.plugins.getJwtConfig
 import io.ktor.server.application.Application
-import io.ktor.server.application.call
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
-import io.ktor.server.response.respondText
-import io.ktor.server.routing.get
-import io.ktor.server.routing.routing
 
 fun main() {
     embeddedServer(Netty, port = SERVER_PORT, host = "0.0.0.0", module = Application::module)
@@ -16,9 +15,8 @@ fun main() {
 }
 
 fun Application.module() {
-    routing {
-        get("/") {
-            call.respondText("Ktor: ${Greeting().greet()}")
-        }
-    }
+    val jwtConfig = getJwtConfig()
+    configureSerialization()
+    configureSecurity(jwtConfig)
+    configureRouting(jwtConfig)
 }

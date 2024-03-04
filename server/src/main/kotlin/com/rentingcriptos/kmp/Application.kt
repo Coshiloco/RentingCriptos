366741ca7ff765ff56fe949ef407e4382c070ce3
Coshiloco <pablohurtadohg@gmail.com>
2024-03-04 02:21:40 +0100

    Fixed

--- server/src/main/kotlin/com/rentingcriptos/kmp/Application.kt
+++ server/src/main/kotlin/com/rentingcriptos/kmp/Application.kt
@@ -1,22 +1,15 @@
 package com.rentingcriptos.kmp
 
-import SERVER_PORT
 import com.rentingcriptos.kmp.plugins.configureRouting
 import com.rentingcriptos.kmp.plugins.configureSecurity
-import com.rentingcriptos.kmp.plugins.configureSerialization
 import com.rentingcriptos.kmp.plugins.getJwtConfig
 import io.ktor.server.application.Application
-import io.ktor.server.engine.embeddedServer
-import io.ktor.server.netty.Netty
+import io.ktor.server.netty.EngineMain
 
-fun main() {
-    embeddedServer(Netty, port = SERVER_PORT, host = "0.0.0.0", module = Application::module)
-        .start(wait = true)
-}
+fun main(args: Array<String>): Unit = EngineMain.main(args)
 
 fun Application.module() {
     val jwtConfig = getJwtConfig()
-    configureSerialization()
     configureSecurity(jwtConfig)
     configureRouting(jwtConfig)
 }

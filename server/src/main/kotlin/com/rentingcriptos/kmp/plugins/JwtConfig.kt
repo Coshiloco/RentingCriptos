511c0fb240914cb771faec9a24bd948446c54dac
Coshiloco <pablohurtadohg@gmail.com>
2024-03-04 02:16:30 +0100

    Commit to Auth

--- server/src/main/kotlin/com/rentingcriptos/kmp/plugins/JwtConfig.kt
+++ server/src/main/kotlin/com/rentingcriptos/kmp/plugins/JwtConfig.kt
@@ -5,11 +5,9 @@ import io.ktor.server.application.Application
 
 
 fun Application.getJwtConfig(): JwtConfig {
-    val secret =
-        environment.config.propertyOrNull("ktor.jwt.secret")?.getString() ?: "default_secret"
-    val issuer = environment.config.property("ktor.jwt.issuer").getString()
-    val audience = environment.config.property("ktor.jwt.audience").getString()
-    val realm = environment.config.property("ktor.jwt.realm").getString()
-
-    return JwtConfig(secret, issuer, audience, realm)
+    val secret = environment.config.property("jwt.secret").getString()
+    val issuer = environment.config.property("jwt.issuer").getString()
+    val audience = environment.config.property("jwt.audience").getString()
+    val myRealm = environment.config.property("jwt.realm").getString()
+    return JwtConfig(secret, issuer, audience, myRealm)
 }

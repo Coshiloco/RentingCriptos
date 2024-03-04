dda6732b0a019d9bb637d43177f46d78c40b9934
Coshiloco <pablohurtadohg@gmail.com>
2024-03-04 13:34:07 +0100

    Serializable problem

--- server/src/main/kotlin/com/rentingcriptos/kmp/auth/models/UserLoginRequest.kt
+++ server/src/main/kotlin/com/rentingcriptos/kmp/auth/models/UserLoginRequest.kt
@@ -1,14 +1,12 @@
 package com.rentingcriptos.kmp.auth.models
 
-import kotlinx.serialization.Serializable
 
-@Serializable
 data class LoginRequest(
     val username: String,
     val password: String
 )
 
-@Serializable
+
 data class RegisterRequest(
     val username: String,
     val email: String,

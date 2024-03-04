d0c477f6a6fcb414c4c39d33ab1d4f8f446d6509
Coshiloco <pablohurtadohg@gmail.com>
2024-03-04 02:56:24 +0100

    Fixed

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.jetbrainsCompose) apply false
-    alias(libs.plugins.kotlinJvm) apply false
+    kotlin("jvm") version libs.versions.kotlin.get()
+    id("io.ktor.plugin") version libs.versions.ktor.get()
+    id("org.jetbrains.kotlin.plugin.serialization") version libs.versions.kotlinSerialization.get()
     alias(libs.plugins.kotlinMultiplatform) apply false
 }
